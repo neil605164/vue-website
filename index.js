83d21df25f21855d814a1d82155c3bed37a64611
neil_hsieh <neil605164@gmail.com>
2021-07-29 23:12:11 +0800

    [add] main filter tag to show different card and use vue to show progess data and plan data
    [add] input value to fromlist

--- index.js
+++ index.js
@@ -31,37 +31,128 @@ var app = new Vue({
         filter: [{
                 id: 1,
                 icon: "",
-                name: "ALL",
-                status: true
+                name: "ALL"
             },
             {
                 id: 2,
                 icon: "far fa-gem",
-                name: "Design",
-                status: false
+                name: "Design"
             },
             {
                 id: 3,
                 icon: "far fa-images",
-                name: "Photos",
-                status: false
+                name: "Photos"
             },
             {
                 id: 4,
                 icon: "fas fa-map-pin",
-                name: "Art",
-                status: false
+                name: "Art"
             },
         ],
+        cardlist: [{
+                id: 1,
+                image: "/images/montain.jpeg",
+                title: "Lorem Ipsum",
+                content: "Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+                tag: "Design"
+            },
+            {
+                id: 2,
+                image: "/images/lights.jpeg",
+                title: "Lorem Ipsum",
+                content: "Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+                tag: "Photos"
+            }, {
+                id: 3,
+                image: "/images/nature.jpeg",
+                title: "Lorem Ipsum",
+                content: "Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+                tag: "Art"
+            }, {
+                id: 4,
+                image: "/images/p1.jpeg",
+                title: "Lorem Ipsum",
+                content: "Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+                tag: "Design"
+            }, {
+                id: 5,
+                image: "/images/p2.jpeg",
+                title: "Lorem Ipsum",
+                content: "Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+                tag: "Design"
+            }, {
+                id: 6,
+                image: "/images/p3.jpeg",
+                title: "Lorem Ipsum",
+                content: "Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+                tag: "Photos"
+            },
+            {
+                id: 7,
+                image: "/images/montain.jpeg",
+                title: "Lorem Ipsum",
+                content: "Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+                tag: "Photos"
+            },
+        ],
+        photoList: ["/images/avatar_g.jpeg", "/images/me2.jpeg"],
+        filterActive: "ALL",
+        progessList: [{
+            name: "Photography",
+            progess: 95,
+        }, {
+            name: "Web Design",
+            progess: 85,
+        }, {
+            name: "Photoshop",
+            progess: 80,
+        }],
+        formList: {
+            Name: "",
+            Email: "",
+            Message: "",
+        },
+        planList: [{
+                id: 1,
+                title: "Basic",
+                contents: ["Web Design", "Photography", "1GB Storage", "Mail Support"],
+                charge: 10,
+            },
+            {
+                id: 2,
+                title: "Pro",
+                contents: ["Web Design", "Photography", "50GB Storage", "Mail Support"],
+                charge: 25,
+            },
+            {
+                id: 3,
+                title: "Premium",
+                contents: ["Web Design", "Photography", "Unlimited Storage", "Mail Support"],
+                charge: 25,
+            },
+        ],
+        fromList: {
+            name: "",
+            email: "",
+            message: "",
+        }
+    },
+    // 計算,篩選,過濾資料後，在渲染到網頁上
+    computed: {
+        filterCardList() {
+            vm = this
+
+            if (vm.filterActive === "ALL") {
+                return vm.cardlist
+            }
+
+            return vm.cardlist.filter(item => item.tag === vm.filterActive)
+        },
     },
     methods: {
-        // update main filter css active status
-        updateFilterStatus(item) {
-            this.filter.forEach(function(item, i) {
-                item.status = false
-            });
-
-            item.status = true
-        },
+        SendFormData() {
+            vm = this
+            console.log(vm.fromList.email, vm.fromList.message, vm.fromList.name, "Send Successfully")
+        }
     },
 })
